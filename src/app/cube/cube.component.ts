b49303c83349b6bef09de063129f2b27d015bfe8
Gavin Wang <ooboqoo@gmail.com>
2017-02-13 22:11:28 +0800

    add three.js cube demo (with --aot support)

---
+++ src/app/cube/cube.component.ts
@@ -0,0 +1,64 @@
+import { Component, ElementRef, Input, ViewChild, AfterViewInit } from '@angular/core';
+import * as THREE from 'three';
+
+@Component({
+  selector: 'app-cube',
+  templateUrl: './cube.component.html',
+  styleUrls: ['./cube.component.scss']
+})
+export class CubeComponent implements AfterViewInit {
+  @ViewChild('canvas') private canvasRef: ElementRef;
+
+  private get canvas(): HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
+
+  private renderer: THREE.WebGLRenderer;
+  private scene: THREE.Scene;
+  private camera: THREE.PerspectiveCamera;
+  private cube: THREE.Mesh;
+
+
+  /* CUBE PROPERTIES */
+  @Input() public rotationSpeedX = 0.005;
+  @Input() public rotationSpeedY = 0.01;
+  @Input() public size = 200;
+
+  /* STAGE PROPERTIES */
+  @Input() public cameraZ = 400;
+
+  private init() {
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    this.camera.position.z = 1000;
+
+    const geometry = new THREE.BoxGeometry( 200, 200, 200 );
+    const material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+    this.cube = new THREE.Mesh( geometry, material );
+    this.scene.add( this.cube );
+
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    this.renderer.setSize( 800, 800 );  // 会改变 canvas 尺寸大小
+  }
+
+  private animateCube() {
+    this.cube.rotation.x += this.rotationSpeedX;
+    this.cube.rotation.y += this.rotationSpeedY;
+  }
+
+  private startRenderingLoop() {
+    const component: CubeComponent = this;
+    (function render() {
+      requestAnimationFrame(render);
+      component.animateCube();
+      component.renderer.render(component.scene, component.camera);
+    }());
+  }
+
+  ngAfterViewInit() {
+    this.init();
+    this.startRenderingLoop();
+  }
+}
