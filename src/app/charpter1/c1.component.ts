2aed9eda5deadf7a678d1be8493381b706182a44
Gavin Wang <ooboqoo@gmail.com>
2017-02-15 11:41:08 +0800

    small fix and adjustment

--- src/app/charpter1/c1.component.ts
+++ src/app/charpter1/c1.component.ts
@@ -3,7 +3,6 @@ import {
   Scene, PerspectiveCamera, WebGLRenderer, AxisHelper, CameraHelper,
   Mesh, MeshBasicMaterial, MeshLambertMaterial, PlaneGeometry, BoxGeometry, SphereGeometry,
   SpotLight,
-  Color,
 } from 'three';
 
 @Component({
@@ -38,7 +37,7 @@ export class C1Component implements AfterViewInit {
     this.camera.lookAt(this.scene.position);
 
     this.renderer = new WebGLRenderer({ canvas: this.canvas });
-    this.renderer.setClearColor(0xEEEEEE);
+    this.renderer.setClearColor(0xeeeeee);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.shadowMapEnabled = true;
 
@@ -49,21 +48,21 @@ export class C1Component implements AfterViewInit {
     const planeMaterial = new MeshLambertMaterial({ color: 0xcccccc });
     this.plane = new Mesh(planeGeometry, planeMaterial);
     this.plane.rotation.x = -0.5 * Math.PI;
-    Object.assign(this.plane.position, { x: 15, y: 0, z: 0 });
+    Object.assign(this.plane.position, { x: 10, y: 0, z: 0 });
     this.plane.receiveShadow = true;
     this.scene.add(this.plane);
 
     const cubeGeometry = new BoxGeometry(5, 5, 5);
     const cubeMaterial = new MeshLambertMaterial({ color: 0xff0000, wireframe: false });
     this.cube = new Mesh(cubeGeometry, cubeMaterial);
-    Object.assign(this.cube.position, { x: -3, y: 3, z: 0 });
+    Object.assign(this.cube.position, { x: -8, y: 3, z: 0 });
     this.cube.castShadow = true;
     this.scene.add(this.cube);
 
     const sphereGeometry = new SphereGeometry(4, 20, 20);
-    const sphereMaterial = new MeshBasicMaterial({ color: 0x4444ff, wireframe: false });
+    const sphereMaterial = new MeshLambertMaterial({ color: 0x4444ff, wireframe: false });
     this.sphere = new Mesh(sphereGeometry, sphereMaterial);
-    Object.assign(this.sphere.position, { x: 20, y: 4, z: 2 });
+    Object.assign(this.sphere.position, { x: 15, y: 4, z: 2 });
     this.sphere.castShadow = true;
     this.scene.add(this.sphere);
 
@@ -79,7 +78,7 @@ export class C1Component implements AfterViewInit {
     this.cube.rotation.z += 0.02;
 
     this.step += 0.04;
-    this.sphere.position.x = 20 + ( 10 * (Math.cos(this.step)));
+    this.sphere.position.x = 15 + ( 10 * (Math.cos(this.step)));
     this.sphere.position.y = 2 + ( 10 * Math.abs(Math.sin(this.step)));
   }
 
